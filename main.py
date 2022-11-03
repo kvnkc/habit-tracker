ad2f275d788b7540a248c9803d46ba3f32460c2a
Cheung <cheung@tdfcu.com>
2022-11-03 12:16:54 -0400

    implemented put and delete requests

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import requests
+from datetime import datetime
 
 pixela_endpoint = 'https://pixe.la/v1/users'
 USERNAME = 'kvnkc'
 TOKEN = 'kljdslaj287389421yhuiasd'
+GRAPHID = 'readinggraph'
 
 user_params = {
     'token': TOKEN,
@@ -12,10 +14,11 @@ user_params = {
 }
 
 # response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)
 
 graph_endpoint = f'{pixela_endpoint}/{USERNAME}/graphs'
 graph_config = {
-    'id': 'graph1',
+    'id': GRAPHID,
     'name': 'Reading Graph',
     'unit': 'minutes',
     'type': 'int',
@@ -26,4 +29,37 @@ headers = {
     'X-USER-TOKEN': TOKEN
 }
 
-requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+
+# response = requests.post(
+#     url=graph_endpoint, json=graph_config, headers=headers)
+# print(response.text)
+
+pixel_endpoint = f'{pixela_endpoint}/{USERNAME}/graphs/{GRAPHID}'
+
+today = datetime.now()
+yesterday = datetime(year=2022, month=11, day=2)
+
+pixel_params = {
+    'date': today.strftime('%Y%m%d'),
+    'quantity': '20'
+}
+
+
+# response = requests.post(
+#     url=pixel_endpoint, json=pixel_params, headers=headers)
+# print(response.text)
+
+pixel_update = f'{pixela_endpoint}/{USERNAME}/graphs/{GRAPHID}/{today.strftime("%Y%m%d")}'
+
+update_parameters = {
+    'quantity': '40'
+}
+
+# response = requests.put(
+#     url=pixel_update, json=update_parameters, headers=headers)
+# print(response.text)
+
+pixel_delete = f'{pixela_endpoint}/{USERNAME}/graphs/{GRAPHID}/{yesterday.strftime("%Y%m%d")}'
+
+response = requests.delete(url=pixel_delete, headers=headers)
+print(response.text)
